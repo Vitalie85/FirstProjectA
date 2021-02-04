4637cff8bc9effd8b6759583042d96814160fc8e
Vitalie ADOMNITA <v.adomnita@gmail.com>
2021-02-04 17:18:59 +0200

    Task Grid with FOR loop added

---
+++ src/TaskGrid/taskGrid1.java
@@ -0,0 +1,16 @@
+package TaskGrid;
+
+public class taskGrid1 {
+    public static void main(String[] args) {
+
+        for (int counter = 0; counter < 10; counter++) {
+
+
+            for (int counter1 = 0; counter1 < 10; counter1++) {
+                System.out.print("-");
+            }
+                System.out.println();
+            }
+        }
+    }
+
